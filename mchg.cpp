4acfba2094d7529e1660a4f2dfeeeb3cfee0a2db
Christopher Cole <chris.c.1221@gmail.com>
2017-04-05 10:39:05 -0400

    input options working

--- mchg.cpp
+++ mchg.cpp
@@ -8,23 +8,41 @@ int main( int argc,
 {
     int count;  
     char* gen = NULL;
-  
-    // Display each command-line argument.  
-    cout << "\nCommand-line arguments:\n";  
-    for( count = 0; count < argc; count++ )  
-         cout << "  argv[" << count << "]   "  
-	            << argv[count] << "\n";
+    char* summary = NULL;
+    char* output = NULL;
+    double h2 = 0.5;
+    double pc = 0.5;
+    double pnc = 0.5;
+    double nc = 0.5;
+    bool maf = false; 
+    double maf_range = 0;
+    int nrep = 1; 
+    
+    for (count = 1; count < argc; count+=2 )
+	if( ! strcmp(argv[count], "--gen") ){
+		gen = argv[count+1];
+	}else if( ! strcmp(argv[count], "--summary") ){
+		summary = argv[count+1];
+	}else if( ! strcmp(argv[count], "--output") ){
+		output = argv[count+1];
+	}else if( ! strcmp(argv[count], "--h2") ){
+		h2 = strtod(argv[count+1], NULL);	
+	}else if( ! strcmp(argv[count], "--pc") ){
+		pc = strtod(argv[count+1], NULL);
+	}else if( ! strcmp(argv[count], "--pnc") ){
+		pnc = strtod(argv[count+1], NULL);
+	}else if( ! strcmp(argv[count], "--nc") ){
+		nc = strtod(argv[count+1], NULL);
+	}else if( ! strcmp(argv[count], "--maf") ){
+		maf = strcmp(argv[count+1],"true");
+	}else if( ! strcmp(argv[count], "--maf-range") ){
+		maf_range = strtod(argv[count+1], NULL);
+	}else if( ! strcmp(argv[count], "--nrep") ){
+		nrep = strtod(argv[count+1], NULL);
+	} else {
+		cout << "\n" << argv[count] << " is an unrecognized tag, please read the documentation for more details." << "\n";
+		exit(EXIT_FAILURE);
+	}
 
-    // Parse command line arguements to variables
-
-    for (count = 0; count < argc; count++ )
-	//char arg = argv[count];	
-	//std::string str(arg);
-	//std::string args( argv[count] );
-	char* dat = argv[count];
-    	char* argument = "--gen";
-	if(dat == argument)
-		cout << argv[count];
-
-
+    cout << pc << endl;
 }
