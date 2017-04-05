18c4e71e40e23322c184c98e15733ae89d01c519
Christopher Cole <chris.c.1221@gmail.com>
2017-04-05 11:17:49 -0400

    header

--- mchg.cpp
+++ mchg.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <armadillo>
+#include <time.h>
 
 using namespace std;
 int main( int argc,
@@ -18,6 +19,22 @@ int main( int argc,
     double maf_range = 0;
     int nrep = 1; 
     
+    cout << 
+	"@ ----------------------------------------- @ \n" <<
+	"|  				             | \n" <<
+	"|    	      Multiple Comparrisons          | \n" <<
+	"|             in the Human Genome           | \n" <<
+	"|   					     | \n" <<
+	"|          Website and Quick Start:	     | \n" <<
+	"|      https://github.com/Chris1221/mchg    | \n" <<
+	"| 				             | \n" <<
+	"|            Christpher Cole 2017	     | \n" <<
+	"|					     | \n" <<
+	"@ ----------------------------------------- @ \n" << "\n";
+
+    time_t current_time;
+    char* c_time_string;
+
     for (count = 1; count < argc; count+=2 )
 	if( ! strcmp(argv[count], "--gen") ){
 		gen = argv[count+1];
@@ -44,5 +61,20 @@ int main( int argc,
 		exit(EXIT_FAILURE);
 	}
 
-    cout << pc << endl;
+    cout << "Input options: " << "\n" <<
+	    "    .gen file: \t \t" << gen << "\n" <<
+	    "    .summary file: \t" << summary << "\n\n" <<
+	    "    Heritability: \t" << h2 << "\n" <<
+	    "    % Causal: \t\t" << pc << "\n" <<
+    	    "    % Non-Causal: \t" << pnc << "\n" <<
+	    "    Number of Causal: \t" << nc << "\n\n" <<
+	    "    MAF: \t\t" << maf << "\n" <<
+	    "    MAF Range: \t\t" << maf_range << "\n\n" <<
+	    "    # Permuations: \t" << nrep << "\n\n" <<
+	    "    Output File: \t" << output << "\n\n";
+	
+    current_time = time(NULL);
+    c_time_string = ctime(&current_time);
+
+    cout << "Analysis started at " << c_time_string << "\n";
 }
